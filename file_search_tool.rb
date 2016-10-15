242aa9255f8a5d96d24cd53b5123f59aece50cb2
Michael Cain <michaelmcain@yahoo.com>
2016-10-15 11:43:51 -0400

    added separate spec file

--- file_search_tool.rb
+++ file_search_tool.rb
@@ -32,28 +32,7 @@ class FileSearchTool
     results = "There are #{count} of strict instances of #{query.upcase} in this file"
     @file.rewind
     results
-  end
 
-end
-
-RSpec.describe FileSearchTool do
-
-  describe 'Searches file for query matches' do
-
-    let(:file) 		{File.new("#{Dir.pwd}/test_file.txt")}
-    let(:query) 	{'test'}
-
-    it 'parses file for lazy matches' do
-      search = FileSearchTool.new(file, query)
-      results = search.lazy_matcher
-      expect(results).to include('3')
-    end
-
-    it 'parses file for strict matches' do
-      search = FileSearchTool.new(file, query)
-      results = search.strict_matcher
-      expect(results).to include('2')
-    end
 
   end
 
